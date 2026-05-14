a5bd95f97fa2034f6e8f71b62ecf4a42c6089ce4
Artur Pegoretti <pegorettiartur@gmail.com>
2026-05-13 22:36:25 -0300

    Add files via upload

---
+++ aula 14/javascript.js
@@ -0,0 +1,28 @@
+var x = 10
+if ( x > 5 ) {
+   console.log("x is greater than 5") 
+} else if (x < 5) {
+    console.log("x is lees tham 5")
+} else {
+    console.log("x is equal to 5")
+}
+
+for (let i = 0; i < 10; i++) {
+if (i% 2 == 0) {
+    console.log(i);
+}
+} 
+
+for (let i = 0; i < 10; i+= 2) {
+ console.log(i);
+}
+
+for (let i = 1; i < 10; i+= 2) {
+ console.log(i);
+}
+
+var y = 0;
+while (y <10) {
+    console.log(y); 
+    y++;
+}
